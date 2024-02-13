d4237534308a0b30c89dc907f215753fb5e9b480
Desier Kylian <kylian.desier@math.u-bordeaux.fr>
2024-02-13 12:11:05 +0100

    Get rid of preprocessing

--- Docker/run_pipeline.sh
+++ Docker/run_pipeline.sh
@@ -12,36 +12,36 @@ MODEL_FOLDER=$DATA_FOLDER'/MODEL'
 registration_folder=$OUTPUT_FOLDER'/Registered'
 mkdir -p $registration_folder
 
-for pat_dir in $INPUT_FOLDER/*
-do
-    patient_name="$(basename $pat_dir)"
-    echo "Registering scans for patient $patient_name"
-    pat_registered_folder=$registration_folder/$patient_name
-    echo $pat_registered_folder
-    mkdir -p $pat_registered_folder
-    for scan in $pat_dir/*.nii.gz
-    do
-        scan_name="$(basename $scan)"
-        echo "Registering scan $scan_name"
-        if [[ $scan == *"T2.nii.gz"* ]] || [[ $scan == *"FLAIR.nii.gz"* ]] ; then
-            atlas_file=$ATLAS_DATA_FOLDER/T2_atlas.nii.gz
-        fi
+# for pat_dir in $INPUT_FOLDER/*
+# do
+#     patient_name="$(basename $pat_dir)"
+#     echo "Registering scans for patient $patient_name"
+#     pat_registered_folder=$registration_folder/$patient_name
+#     echo $pat_registered_folder
+#     mkdir -p $pat_registered_folder
+#     for scan in $pat_dir/*.nii.gz
+#     do
+#         scan_name="$(basename $scan)"
+#         echo "Registering scan $scan_name"
+#         if [[ $scan == *"T2.nii.gz"* ]] || [[ $scan == *"FLAIR.nii.gz"* ]] ; then
+#             atlas_file=$ATLAS_DATA_FOLDER/T2_atlas.nii.gz
+#         fi
 
-        if [[ $scan == *"T1GD.nii.gz"* ]] || [[ $scan == *"T1.nii.gz"* ]] ; then
-            atlas_file=$ATLAS_DATA_FOLDER/T1_atlas.nii.gz
-        fi
+#         if [[ $scan == *"T1GD.nii.gz"* ]] || [[ $scan == *"T1.nii.gz"* ]] ; then
+#             atlas_file=$ATLAS_DATA_FOLDER/T1_atlas.nii.gz
+#         fi
 
-        elastix -f $atlas_file -m $scan -out $pat_registered_folder -p $ELASTIX_DATA_FOLDER/parameter_map_0.txt -p $ELASTIX_DATA_FOLDER/parameter_map_1.txt
-        transformix -in $scan -out $pat_registered_folder -tp $pat_registered_folder/TransformParameters.1.txt
+#         elastix -f $atlas_file -m $scan -out $pat_registered_folder -p $ELASTIX_DATA_FOLDER/parameter_map_0.txt -p $ELASTIX_DATA_FOLDER/parameter_map_1.txt
+#         transformix -in $scan -out $pat_registered_folder -tp $pat_registered_folder/TransformParameters.1.txt
 
-        mv $pat_registered_folder/result.nii.gz $pat_registered_folder/$scan_name
+#         mv $pat_registered_folder/result.nii.gz $pat_registered_folder/$scan_name
 
-        atlas_file="NONE"
-        rm $pat_registered_folder/*.txt
-        rm $pat_registered_folder/*.log
+#         atlas_file="NONE"
+#         rm $pat_registered_folder/*.txt
+#         rm $pat_registered_folder/*.log
 
-    done
-done
+#     done
+# done
 
 prediction_output_folder=$OUTPUT_FOLDER/Predictions
 mkdir -p $prediction_output_folder
